afdfff82c7a34e19fcfa22678e49f74ef7670b2e
Albin <56157868+albinpk@users.noreply.github.com>
2025-05-29 23:05:15 +0530

    feat: curl configurations

--- packages/rhttp_curl_logger/lib/src/rhttp_curl_logger.dart
+++ packages/rhttp_curl_logger/lib/src/rhttp_curl_logger.dart
@@ -6,21 +6,61 @@ import 'package:rhttp/rhttp.dart';
 /// Prints the request as a single line cURL command
 class RhttpCurlLogger extends Interceptor {
   /// Creates a new [RhttpCurlLogger].
-  RhttpCurlLogger();
+  RhttpCurlLogger({
+    this.useDoubleQuotes = true,
+    this.escapeQuotesInBody = true,
+    this.multiline = false,
+    this.logName,
+    this.logger,
+  });
+
+  /// Whether to use double quotes or not. Defaults to `true`.
+  final bool useDoubleQuotes;
+
+  /// Whether to escape quotes in the body or not. Defaults to `true`.
+  final bool escapeQuotesInBody;
+
+  /// Whether to log curl command in multiline or not. Defaults to `false`.
+  final bool multiline;
+
+  /// The name of the logger. Defaults to `curl_log`.
+  /// This will be ignored if [logger] is not null.
+  final String? logName;
+
+  /// A function to handle the curl command.
+  ///
+  /// If not provided, the curl command will be printed
+  /// to the console using the [log] function.
+  final void Function(String curl)? logger;
 
   @override
   Future<InterceptorResult<HttpRequest>> beforeRequest(
     HttpRequest request,
   ) async {
-    _printCurlCommand(request);
+    _handleCurl(request);
     return Interceptor.next(request);
   }
 
-  void _printCurlCommand(HttpRequest request) {
-    log(_buildSingleLineCurlCommand(request), name: 'RHTTP CURL');
+  void _handleCurl(HttpRequest request) {
+    final String curl;
+    try {
+      curl = _buildCurl(request);
+      // ignore: avoid_catches_without_on_clauses
+    } catch (e) {
+      log('Error building curl command', error: e);
+      return;
+    }
+    if (logger != null) {
+      logger!(curl);
+    } else {
+      log(curl, name: logName ?? 'curl_log');
+    }
   }
 
-  String _buildSingleLineCurlCommand(HttpRequest request) {
+  String _buildCurl(HttpRequest request) {
+    final q = useDoubleQuotes ? '"' : "'"; // quote
+    final s = multiline ? '\n' : ' '; // separator
+
     final buffer = StringBuffer(
       'curl -X ${request.method.value.toUpperCase()}',
     );
@@ -28,7 +68,9 @@ class RhttpCurlLogger extends Interceptor {
     // Append headers
     request.headers?.toMapList().forEach((name, values) {
       for (final value in values) {
-        buffer.write(' -H "$name: $value"');
+        buffer
+          ..write(s)
+          ..write('-H $q$name: $value$q');
       }
     });
 
@@ -40,10 +82,12 @@ class RhttpCurlLogger extends Interceptor {
       bodyString = request.body.toString();
     }
     if (bodyString != null) {
-      // Escape for shell command - use different strategies based on shell
-      // For most shells, escape double quotes
-      final escapedBody = bodyString.replaceAll('"', r'\"');
-      buffer.write(' -d "$escapedBody"');
+      final data = escapeQuotesInBody
+          ? bodyString.replaceAll(q, '\\$q')
+          : bodyString;
+      buffer
+        ..write(s)
+        ..write('-d $q$data$q');
     }
 
     // Append query parameters to the URL
@@ -55,7 +99,10 @@ class RhttpCurlLogger extends Interceptor {
         '${request.settings?.baseUrl ?? ""}${request.url}'
         '${queryParams != null ? '?$queryParams' : ''}';
 
-    buffer.write(' "$url"');
+    buffer
+      ..write(s)
+      ..write('$q$url$q');
+
     return buffer.toString();
   }
 }
